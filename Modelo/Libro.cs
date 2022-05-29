385b26b9c3439aa32203c28103a930cc38bfff8d
Juan Pablo Mateus Pardo <78219039+juanpablo130502@users.noreply.github.com>
2022-05-28 21:32:11 -0500

    Add files via upload

---
+++ Modelo/Libro.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace Biblioteca.Api.Modelo
+{
+    public class Libro
+    {
+        [Key]
+        public int IdLibro { get; set; }
+        public string Titulo { get; set; }
+        public string Editorial { get; set; }
+        public string Autor { get; set; }
+        public int Pagina { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
