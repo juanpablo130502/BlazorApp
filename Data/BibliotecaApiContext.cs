4cb2f98fd422a04a226337655bc181fac5a07868
Juan Pablo Mateus Pardo <78219039+juanpablo130502@users.noreply.github.com>
2022-05-28 21:30:17 -0500

    Add files via upload

---
+++ Data/BibliotecaApiContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Biblioteca.Api.Modelo;
+
+namespace Biblioteca.Api.Data
+{
+    public class BibliotecaApiContext : DbContext
+    {
+        public BibliotecaApiContext (DbContextOptions<BibliotecaApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Biblioteca.Api.Modelo.Libro> Libro { get; set; }
+    }
+}
