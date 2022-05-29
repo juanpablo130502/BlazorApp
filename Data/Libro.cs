9c0920c0bb193368bc8fc162b929f747535e85b9
Juan Pablo Mateus Pardo <78219039+juanpablo130502@users.noreply.github.com>
2022-05-28 21:16:17 -0500

    Add files via upload

---
+++ Data/Libro.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp1.Data
+{
+    public class Libro
+    {
+        [Key]
+        public int idlibro { get; set; }
+        public string titulo { get; set; }
+        public string editorial { get; set; }
+        public string autor { get; set; }
+        public int pagina { get; set; }
+        public string descripcion { get; set; }
+    }
+}
