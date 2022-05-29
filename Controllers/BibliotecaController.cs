f224cba418162bf4f3f185febf4a1e4c611c027d
Juan Pablo Mateus Pardo <78219039+juanpablo130502@users.noreply.github.com>
2022-05-28 21:29:36 -0500

    Add files via upload

---
+++ Controllers/BibliotecaController.cs
@@ -0,0 +1,55 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Biblioteca.Api.Data;
+using Biblioteca.Api.Modelo;
+
+namespace Biblioteca.Api.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class BibliotecaController : ControllerBase
+    {
+        private readonly BibliotecaApiContext _context;
+
+
+        public BibliotecaController(BibliotecaApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ConsultaLibro()
+        {
+            return Ok(await _context.Libro.ToListAsync());
+        }
+        [HttpPost]
+        public async Task<IActionResult> AgregarLibro(Libro libro)
+        {
+            _context.Libro.Add(libro);
+            await _context.SaveChangesAsync();
+            return Ok(libro);
+        }
+        [HttpPut]
+        public async Task<IActionResult> ActualizarLibro(Libro libro)
+        {
+            _context.Update(libro);
+            await _context.SaveChangesAsync();
+            return Ok(libro);
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteLibro(Libro libro)
+        {
+            _context.Remove(libro);
+            await _context.SaveChangesAsync();
+            return Ok(libro);
+        }
+
+       
+        
+    }
+}
